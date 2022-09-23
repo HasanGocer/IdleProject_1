2836c9eb6046c7db7c95132e0e39472c5a535d67
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2022-09-23 11:48:17 +0300

    Added Merge Mechanics

---
+++ Assets/Scripts/MergeMechanic.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MergeMechanic : MonoSingleton<MergeMechanic>
+{
+    public int OPLastCount;
+    [SerializeField] private int _OPShotCount;
+    [SerializeField] private float objectDistance;
+
+    public void Merge(GameObject objectShot, GameObject objectMerge, int OPMergeCount)
+    {
+        ObjectPool.Instance.AddObject(_OPShotCount, objectShot);
+        GameObject obj1 = ObjectPool.Instance.GetPooledObject(OPMergeCount - 1);
+        GameObject obj2 = ObjectPool.Instance.GetPooledObject(OPMergeCount - 1);
+
+        ObjectPool.Instance.AddObject(OPMergeCount, objectMerge);
+
+        obj1.transform.position = new Vector3(objectShot.transform.position.x + objectDistance, objectShot.transform.position.y, objectShot.transform.position.x - objectDistance);
+        obj2.transform.position = new Vector3(objectShot.transform.position.x + objectDistance, objectShot.transform.position.y, objectShot.transform.position.x - objectDistance);
+
+    }
+
+    public void LastMerge(GameObject other)
+    {
+        ObjectPool.Instance.AddObject(OPLastCount, other);
+    }
+}
---
+++ Assets/Scripts/MergeObject.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MergeObject : MonoBehaviour
+{
+    [SerializeField] private int _OPCount;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Shot"))
+        {
+            if (_OPCount == MergeMechanic.Instance.OPLastCount)
+            {
+                MergeMechanic.Instance.LastMerge(collision.gameObject);
+            }
+            else
+            {
+                MergeMechanic.Instance.Merge(collision.gameObject, this.gameObject, _OPCount);
+            }
+        }
+    }
+}
