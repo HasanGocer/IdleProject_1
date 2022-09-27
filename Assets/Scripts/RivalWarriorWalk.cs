9e0d80a43d40908f296a2e4019fcf147480fdb01
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2022-09-27 12:54:48 +0300

    HG

--- Assets/Scripts/RivalWarriorWalk.cs
+++ Assets/Scripts/RivalWarriorWalk.cs
@@ -3,31 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class RivalWarriorWalk : MonoBehaviour
+public class RivalWarriorWalk : MonoSingleton<RivalWarriorWalk>
 {
-    [SerializeField] private GameObject focusWarrior;
+    //Bu kodda rical Warrior spawn ediliyor saldýracaðý warrior seçiliyor ve harekete geçiriliyor
+    //HATA= bir warriora birden fazla rival Warrior gidiyor
+
     [SerializeField] private int OPRivalWarriorCount;
     [SerializeField] private float rivalWalkTime;
+    [SerializeField] private float rivalCountDawn;
+    [SerializeField] private bool focusOnWarrior;
 
+    private GameObject focusWarrior;
     private GameObject rivalWariror;
     private float minDistance = 10000f;
 
-    private void Update()
+    public IEnumerator RivalWarriorWalkEnumerator()
     {
-        if (Input.GetKeyDown(KeyCode.K))
+        while (true)
         {
-            WalkSet();
+            for (int i = 0; i < WarriorÝnstantiate.Instance.WarriorObject.Count; i++)
+            {
+                if (!WarriorÝnstantiate.Instance.WarriorBool[i])
+                {
+                    focusOnWarrior = true;
+                }
+            }
+            if (GameManager.Instance.inFight && WarriorÝnstantiate.Instance.WarriorObject.Count > 0 && focusOnWarrior)
+            {
+                SpawnRivalWarrior();
+                SetRival();
+                WalkWarrior();
+                focusOnWarrior = false;
+            }
+            yield return new WaitForSeconds(rivalCountDawn);
         }
-    }
-    public void WalkSet()
-    {
-        if (WarriorÝnstantiate.Instance.WarriorObject.Count > 0)
-        {
-            SpawnRivalWarrior();
-            SetRival();
-            WalkWarrior();
-        }
-
     }
 
     private void SpawnRivalWarrior()
@@ -42,11 +51,12 @@ public class RivalWarriorWalk : MonoBehaviour
         {
             if (minDistance > Vector3.Distance(WarriorÝnstantiate.Instance.WarriorObject[i].transform.position, WarriorStatManager.Instance.CastlePos.transform.position) && !WarriorÝnstantiate.Instance.WarriorBool[i])
             {
+                WarriorÝnstantiate.Instance.WarriorBool[i] = true;
                 minDistance = Vector3.Distance(WarriorÝnstantiate.Instance.WarriorObject[i].transform.position, WarriorStatManager.Instance.CastlePos.transform.position);
                 focusWarrior = WarriorÝnstantiate.Instance.WarriorObject[i];
-                WarriorÝnstantiate.Instance.WarriorBool[i] = true;
             }
         }
+        minDistance = 1000f;
     }
 
     private void WalkWarrior()
